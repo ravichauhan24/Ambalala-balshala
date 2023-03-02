5375a1345da2adbb525faa82aabdd08ecc82a6d9
Ravi Chauhan <ravi87104@gmail.com>
2023-03-02 15:15:01 +0530

    api service

--- src/core/services/api.service.ts
+++ src/core/services/api.service.ts
@@ -7,7 +7,7 @@ declare var $: any;
 })
 export class ApiService {
   public static HOST_URL: string = "http://localhost:9000";
-  // public static HOST_URL: string = "http://31.220.111.153:5555";
+  // public static HOST_URL: string = "https://bapsanandmandir.co.in";
   toasts: any[] = [];
   constructor(
     private http: HttpClient,
